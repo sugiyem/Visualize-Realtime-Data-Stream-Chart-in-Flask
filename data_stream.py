bf95b35a45d6c12e3a0f2fb4b609efb50bd4d4db
Farrel Dwireswara Salim <90848662+sugiyem@users.noreply.github.com>
2024-05-15 20:57:58 +0800

    Reorganize some code

--- data_stream.py
+++ data_stream.py
@@ -1,5 +1,4 @@
 from threading import Thread
-import flask_handler
 from datetime import datetime
 import time
 import threading
@@ -32,17 +31,18 @@ class Config():
         self.ymax = ymax
 
 class DataStream(Thread):
-    def __init__(self, _config, data_queue):
+    def __init__(self, _config, data_queue, socketio):
         super(DataStream, self).__init__()
         self.data_queue = data_queue
         self.config = _config
+        self.socketio = socketio
 
     def run(self):
         print('Data stream running')
-        while not flask_handler.thread_stop_event.isSet():
+        while True:
             if not self.data_queue.empty():
                 timestamp,x, y, lon, lat, heigh, rtk, hrms, vhrms = self.data_queue.get()
-                flask_handler.socketio.emit('server',{
+                self.socketio.emit('server',{
                     'id':self.config.id,
                     'time': [timestamp],
                     'x': [x],
@@ -70,37 +70,3 @@ class DataStream(Thread):
                 })
             
             time.sleep(self.config.delay)
-
-
-def def_param(vari, deff):
-    if vari is None:
-        return deff
-    else:
-        return vari
-
-def send_request(id, data, type = 'line', active_points = 20, _label="Value", _legend="data", _width = 200, _height = 100, _name = "Graph", backgroundColor = "rgb(255, 99, 132)", borderColor = "rgb(255, 99, 132)", fill = "false"):
-    type = def_param(type, 'line')
-    active_points = def_param(active_points, 20)
-    _label = def_param(_label, ['Value'])
-    _legend = def_param(_legend, ['data'])
-    _height = def_param(_height, 200)
-    _width = def_param(_width, 100)
-    _name = def_param(_name, 'Graph')
-    fill = def_param(fill, "False")
-    backgroundColor = def_param(backgroundColor, ["rgb(255, 99, 132)"])
-    borderColor = def_param(borderColor, ["rgb(255, 99, 132)"])
-
-    flask_handler.socketio.emit('server',
-    {'id': id,
-    'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-     'value': data,
-     'type': type,
-     'active_points': active_points,
-     'label': _label,
-     'legend': _legend,
-     'name': _name,
-     'width': _width,
-     'height': _height,
-     "backgroundColor": backgroundColor,
-     "borderColor" : borderColor,
-     "fill" : fill})
--- main.py
+++ main.py
@@ -1,8 +1,96 @@
-from flask_handler import start_flask_application
+#from gevent import monkey
+#monkey.patch_all()
 
-"""
-Start Program with this file by running "python3 start.py"
-"""
+#import eventlet
+#eventlet.monkey_patch()
+
+import pandas as pd
+import logging
+
+from flask_socketio import SocketIO
+from flask import Flask, render_template, request, jsonify
+
+from threading import Thread, Event
+from estimator import estimate
+from data_stream import DataStream, Config
+from queue import Queue
+
+
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode='threading')
+
+csv_download_count = 0
+
+data_queue = Queue()
+
+@app.route('/')
+def index():
+    #only by sending this page first will the client be connected to the socketio instance
+    return render_template('index.html')
+
+@app.route('/save_csv', methods=['POST'])
+def save_csv():
+    global csv_download_count
+    csv_download_count += 1
+
+    data = request.get_json()
+    df = pd.DataFrame(data)
+    df.to_csv('output/{}.csv'.format(csv_download_count))
+    
+    return jsonify(message='CSV file saved successfully')
+
+@app.route('/capture', methods=['POST'])
+def capture():
+    data = request.get_json()
+    df = pd.DataFrame(data)
+    displayed_df = df if len(df) < 10 else df.tail(10)
+
+    try:
+        bias, weight, xplot_min, yplot_min, xplot_max, yplot_max = estimate(displayed_df['x'].to_numpy(), displayed_df['y'].to_numpy()) # curve estimator is y = mx + b
+    except Exception as e:
+        return jsonify({'error': str(e)})
+     
+    return jsonify({
+        'bias': bias,
+        'weight': weight,
+        'xplot_min': xplot_min,
+        'yplot_min': yplot_min,
+        'xplot_max': xplot_max,
+        'yplot_max': xplot_max
+    })
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+
+    # Extract values from the incoming request
+    timestamp = data.get('time')
+    x = data.get('x')
+    y = data.get('y')
+    lon = data.get('lon')
+    lat = data.get('lat')
+    heigh = data.get('heigh')
+    rtk = data.get('rtk')
+    hrms = data.get('hrms')
+    vrhms = data.get('vhrms')
+    
+    # Put data into the queue
+    data_queue.put((timestamp, x, y, lon, lat, heigh, rtk, hrms, vrhms))
+    
+    return jsonify(message='Data received successfully')
+
+@socketio.on('connect')
+def connect():
+    # need visibility of the global thread object
+    print('Flask Client connected')
+    loc_config = Config(_name = "Location Stream", xmin = -2.0, xmax = 2.0, ymin = -2.0, ymax = 2.0)
+    DataStream(loc_config, data_queue, socketio).start()
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Flask Client disconnected')
 
 if __name__ == '__main__':
-    start_flask_application()
+    from config_handler import ConfigHandler
+    [HOST,PORT] = ConfigHandler().get_all("Flask") # pylint: disable=unbalanced-tuple-unpacking
+    socketio.run(app, host=HOST, port=PORT) # SocketIOServer
